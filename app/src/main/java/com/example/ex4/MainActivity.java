b677c2405e6a29c62969e7e92aefb2419e80ffec
Atara Razin <atararazin@gmail.com>
2019-06-12 11:22:06 +0300

    sends the port and ip data to the MainActiviy java class properly. also opens up the next page when you click the Connect button. todo: connect as client

--- app/src/main/java/com/example/ex4/MainActivity.java
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.ex4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Connect(View view){
+        EditText editTextIp = (EditText)findViewById(R.id.ip);
+        String ip = editTextIp.getText().toString();
+        System.out.println(ip+"ip");
+        EditText editTextPort = (EditText)findViewById(R.id.port);
+        String portString = editTextPort.getText().toString();
+        int port = Integer.parseInt(portString);
+        System.out.println(port+"port");
+
+        //try{
+         //   Socket client = new Socket(ip,port);
+        //}
+
+
+        Intent intent = new Intent(this, JoystickActivity.class);
+        startActivity(intent);
+        }
+
 }
