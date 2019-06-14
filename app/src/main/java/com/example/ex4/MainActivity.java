b9b9c5e25895616edc29e9c1d4f8a2c5977e79b0
Atara Razin <atararazin@gmail.com>
2019-06-14 19:02:20 +0300

    created the socket. just check to make sure that it connects properly

--- app/src/main/java/com/example/ex4/MainActivity.java
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,21 +23,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Connect(View view){
+        InetAddress serverAddr = null;
         EditText editTextIp = (EditText)findViewById(R.id.ip);
         String ip = editTextIp.getText().toString();
         System.out.println(ip+"ip");
+
         EditText editTextPort = (EditText)findViewById(R.id.port);
         String portString = editTextPort.getText().toString();
         int port = Integer.parseInt(portString);
         System.out.println(port+"port");
 
-        //try{
-         //   Socket client = new Socket(ip,port);
-        //}
+        try{
+            serverAddr = InetAddress.getByName(ip);
+        }
+        catch(Exception e){
+            System.out.println("invalid ip!");
+        }
 
-
+        try{
+            Socket socket = new Socket(serverAddr, port);
+        }
+        catch(Exception e){
+            System.out.println("couldnt open socket!");
+        }
         Intent intent = new Intent(this, JoystickActivity.class);
         startActivity(intent);
         }
+
+    public void sendMessage(final String message) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+
+    public void stopClient() {
+}
+
 
 }
