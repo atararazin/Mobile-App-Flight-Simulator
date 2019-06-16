e18c0f2a5f2575f4b27c7c104c0a6d598b5b2c34
Yossi Weizman <yossi.weizman@gmail.com>
2019-06-17 01:01:49 +0300

    Return the knob to center

--- app/src/main/java/com/example/ex4/JoystickActivity.java
+++ app/src/main/java/com/example/ex4/JoystickActivity.java
@@ -24,7 +24,9 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
     ViewGroup _root;
     private int _xDelta;
     private int _yDelta;
-
+    private int isFirst;
+    private int originalX;
+    private int originalY;
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -32,7 +34,7 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
         super.onWindowFocusChanged(hasFocus);
 
         if(hasFocus) {
-
+            isFirst = 1;
             _root = (ViewGroup)findViewById(R.id.relativeRoot);
             _knob = (ImageView) findViewById(R.id.knob);
 
@@ -55,6 +57,9 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
             RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
             layoutParams.topMargin = (int)_knob.getY() +50;
             layoutParams.leftMargin = (int)_knob.getX() +50;
+
+            originalX = (int)_knob.getX() +50;
+            originalY = (int)_knob.getY() +50;
 
             _knob.setLayoutParams(layoutParams);
             _knob.setOnTouchListener(this);
@@ -105,6 +110,7 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
     public boolean onTouch(View view, MotionEvent event) {
         final int X = (int) event.getRawX();
         final int Y = (int) event.getRawY();
+        Log.d("X" ,String.valueOf(X));
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
                 RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
@@ -112,18 +118,25 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
                 _yDelta = Y - lParams.topMargin;
                 break;
             case MotionEvent.ACTION_UP:
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                layoutParams.topMargin = originalY;
+                layoutParams.leftMargin = originalX;
+                view.setLayoutParams(layoutParams);
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
                 break;
             case MotionEvent.ACTION_POINTER_UP:
                 break;
             case MotionEvent.ACTION_MOVE:
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                layoutParams.leftMargin = X - _xDelta;
-                layoutParams.topMargin = Y - _yDelta;
-                layoutParams.rightMargin = -250;
-                layoutParams.bottomMargin = -250;
-                view.setLayoutParams(layoutParams);
+                RelativeLayout.LayoutParams layoutParams1 = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                layoutParams1.leftMargin = X - _xDelta;
+                layoutParams1.topMargin = Y - _yDelta;
+                layoutParams1.rightMargin = -250;
+                layoutParams1.bottomMargin = -250;
+                //layoutParams.rightMargin = 1000;
+                //layoutParams.bottomMargin = 1000;
+                view.setLayoutParams(layoutParams1);
+                Log.d("ACTION_MOVE" ,String.valueOf(_xDelta));
                 break;
         }
         _root.invalidate();
