ac561b62edf7c9973c649ad1403f63f2ed249d4f
yossiwe <49269899+yossiwe@users.noreply.github.com>
2019-06-17 11:42:09 +0300

    Working3 (#3)
    * Fix joystick sizes
    
    * Fix joystick - now generates values for simulator

--- app/src/main/java/com/example/ex4/JoystickActivity.java
+++ app/src/main/java/com/example/ex4/JoystickActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,6 +17,7 @@ import android.view.Display;
 import android.util.DisplayMetrics;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.content.Context;
 
 public class JoystickActivity extends AppCompatActivity implements View.OnTouchListener {
 
@@ -24,33 +26,39 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
     ViewGroup _root;
     private int _xDelta;
     private int _yDelta;
-    private int isFirst;
     private int originalX;
     private int originalY;
     private int radius;
 
-    private int outerTop;
-    private int outerBot;
 
 
+    // The function checks if a point (x,y) is inside the circle
     private boolean isInCircle(int x, int y)
     {
-        int diffX = originalX - x + 40;
-        int diffY = originalY - y + 280;
+        int diffX = originalX - x;
+        int diffY = originalY - y;
         double d = Math.sqrt( Math.pow(diffX, 2) + Math.pow(diffY, 2));
         return d <= radius;
-
     }
 
-    private boolean isInCircle2(int x, int y)
+    // Gets the Y value for the simulator
+    private double getClientYValue(int currentY)
     {
-        Log.d("outerBot" ,String.valueOf(outerBot));
-        Log.d("outerTop" ,String.valueOf(outerTop));
-        Log.d("x" ,String.valueOf(x));
-        Log.d("y" ,String.valueOf(y));
-        return (y < outerBot) && (y > outerTop);
-
+        double normalY = Math.abs(originalY - currentY) / (double)radius;
+        if (currentY > originalY)
+            normalY = normalY *(-1);
+        return normalY;
     }
+
+    // Gets the X value for the simulator
+    private double getClientXValue(int currentX)
+    {
+        double normalX = Math.abs(originalX - currentX) / (double)radius;
+        if (currentX < originalX)
+            normalX = normalX *(-1);
+        return normalX;
+    }
+
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -58,36 +66,25 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
         super.onWindowFocusChanged(hasFocus);
 
         if(hasFocus) {
-            isFirst = 1;
             _root = (ViewGroup)findViewById(R.id.relativeRoot);
             _knob = (ImageView) findViewById(R.id.knob);
             ImageView outer = (ImageView) findViewById(R.id.outerCircle);
             radius = (outer.getBottom() - outer.getTop()) / 2;
-            outerBot = outer.getBottom();
-            outerTop = outer.getTop();;
-
-            //////////////////////////////////////////////////////////////////////////////
-            Rect offsetViewBounds = new Rect();
-            //returns the visible bounds
-            _knob.getDrawingRect(offsetViewBounds);
-            // calculates the relative coordinates to the parent
-            _root.offsetDescendantRectToMyCoords(_knob, offsetViewBounds);
-
-            int relativeTop = offsetViewBounds.top;
-            int relativeLeft = offsetViewBounds.left;
-
-            //////////////////////////////////////////////////////////////////////////////
 
 
-            int[] pos = new int[2];
-            _knob.getLocationOnScreen(pos);
 
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
-            layoutParams.topMargin = (int)_knob.getY() +50;
-            layoutParams.leftMargin = (int)_knob.getX() +50;
+            // Convert from pixels to dp
+            float factor = getResources().getDisplayMetrics().density;
+            int w  = (int)factor * 98;
+            int h = (int) factor * 114;
 
-            originalX = (int)_knob.getX() +50;
-            originalY = (int)_knob.getY() +50;
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(w, h);
+            layoutParams.topMargin = (int)_knob.getY() +20;
+            layoutParams.leftMargin = (int)_knob.getX() +20;
+
+            // Save the original values for returning to center
+            originalX = (int)_knob.getX() +20;
+            originalY = (int)_knob.getY() +20;
 
             _knob.setLayoutParams(layoutParams);
             _knob.setOnTouchListener(this);
@@ -104,34 +101,49 @@ public class JoystickActivity extends AppCompatActivity implements View.OnTouchL
     public boolean onTouch(View view, MotionEvent event) {
         final int X = (int) event.getRawX();
         final int Y = (int) event.getRawY();
-        //Log.d("X" ,String.valueOf(X));
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
                 RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
                 _xDelta = X - lParams.leftMargin;
                 _yDelta = Y - lParams.topMargin;
                 break;
+
             case MotionEvent.ACTION_UP:
                 RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
                 layoutParams.topMargin = originalY;
                 layoutParams.leftMargin = originalX;
                 view.setLayoutParams(layoutParams);
                 break;
+
             case MotionEvent.ACTION_POINTER_DOWN:
                 break;
+
             case MotionEvent.ACTION_POINTER_UP:
                 break;
+
             case MotionEvent.ACTION_MOVE:
                 RelativeLayout.LayoutParams layoutParams1 = (RelativeLayout.LayoutParams) view.getLayoutParams();
                 layoutParams1.leftMargin = X - _xDelta;
                 layoutParams1.topMargin = Y - _yDelta;
                 layoutParams1.rightMargin = -250;
                 layoutParams1.bottomMargin = -250;
-                if (isInCircle(X,Y))
+
+                // Get values for the simulator
+                double simulatorX = getClientXValue(layoutParams1.leftMargin);
+                double simulatorY = getClientYValue(layoutParams1.topMargin);
+
+                if (isInCircle(layoutParams1.leftMargin, layoutParams1.topMargin) && Math.abs(simulatorX) <= 1 && Math.abs(simulatorY) <= 1)
                 {
+                    Log.d("simulatorX" ,String.valueOf(simulatorX));
+                    Log.d("simulatorY" ,String.valueOf(simulatorY));
                     view.setLayoutParams(layoutParams1);
-                    //Log.d("ACTION_MOVE" ,String.valueOf(_xDelta));
                 }
+
+                // TODO:
+                // Send the values simulatorX, simulatorY to the simulator!
+                //
+
+
 
                 break;
         }
