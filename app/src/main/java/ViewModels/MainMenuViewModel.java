b5d4e72172e5108a651c35910dbac71dc96ec053
Atara Razin <atararazin@gmail.com>
2019-06-19 14:02:33 +0300

    todo: the datastream is returning null. need to fix that

--- app/src/main/java/ViewModels/MainMenuViewModel.java
+++
@@ -1,7 +0,0 @@
-package ViewModels;
-
-public class MainMenuViewModel {
-    public void OnConnect(){
-        System.out.print("got here");
-    }
-}
--- app/src/main/java/com/example/ex4/MainActivity.java
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -10,11 +10,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    private Socket socket;
+    private String ip;
+    private int port;
+    TcpClient tcpClient;
+    private Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +34,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Connect(View view){
-        EditText editTextIp = (EditText)findViewById(R.id.ip);
+    public void Connect(View view) {
+        EditText editTextIp = (EditText) findViewById(R.id.ipString);
         String ip = editTextIp.getText().toString();
 
-        EditText editTextPort = (EditText)findViewById(R.id.port);
+        EditText editTextPort = (EditText) findViewById(R.id.editPort);
         String portString = editTextPort.getText().toString();
         int port = Integer.parseInt(portString);
 
-        ConnectTask connectTask = new ConnectTask(ip,port);
-        connectTask.execute();
+        this.tcpClient = new TcpClient(ip,port);
+        ConnectTask connectTask = new ConnectTask(tcpClient);
+        connectTask.execute("");
 
+        MessageSender messageSender = new MessageSender(this.tcpClient);
+        Thread thread = new Thread(messageSender);
+        thread.start();
+
+        //this.tcpClient.startQueue();
+        messageSender.addToQueue("testing1");
+        messageSender.addToQueue("testing2");
+        messageSender.addToQueue("testing3");
+        messageSender.addToQueue("testing4");
+        System.out.println("sent!!");
 
         Intent intent = new Intent(this, JoystickActivity.class);
         startActivity(intent);
-        }
+    }
 }
